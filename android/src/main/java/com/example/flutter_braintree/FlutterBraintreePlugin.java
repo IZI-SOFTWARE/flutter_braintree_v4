6425b5c3c8fbd9b314c17ca3abe9a0d3102f1222
phongtran <thanhphong190@gmail.com>
2025-07-16 09:17:39 +0700

    update sdk

--- android/src/main/java/com/example/flutter_braintree/FlutterBraintreePlugin.java
+++ android/src/main/java/com/example/flutter_braintree/FlutterBraintreePlugin.java
@@ -8,6 +8,8 @@ import com.braintreepayments.api.PayPalVaultRequest;
 
 import java.util.Map;
 
+import androidx.annotation.NonNull;
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
@@ -16,64 +18,48 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.ActivityResultListener;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 public class FlutterBraintreePlugin implements FlutterPlugin, ActivityAware, MethodCallHandler, ActivityResultListener {
     private static final int CUSTOM_ACTIVITY_REQUEST_CODE = 0x420;
 
     private Activity activity;
     private Result activeResult;
-
-//    private FlutterBraintreeDropIn dropIn;
-
-    public static void registerWith(Registrar registrar) {
-//        FlutterBraintreeDropIn.registerWith(registrar);
-        final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_braintree.custom");
-        FlutterBraintreePlugin plugin = new FlutterBraintreePlugin();
-        plugin.activity = registrar.activity();
-        registrar.addActivityResultListener(plugin);
-        channel.setMethodCallHandler(plugin);
-    }
+    private MethodChannel channel;
 
     @Override
-    public void onAttachedToEngine(FlutterPluginBinding binding) {
-        final MethodChannel channel = new MethodChannel(binding.getBinaryMessenger(), "flutter_braintree.custom");
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+        channel = new MethodChannel(binding.getBinaryMessenger(), "flutter_braintree.custom");
         channel.setMethodCallHandler(this);
-
-//        dropIn = new FlutterBraintreeDropIn();
-//        dropIn.onAttachedToEngine(binding);
     }
 
     @Override
-    public void onDetachedFromEngine(FlutterPluginBinding binding) {
-//        dropIn.onDetachedFromEngine(binding);
-//        dropIn = null;
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        if (channel != null) {
+            channel.setMethodCallHandler(null);
+            channel = null;
+        }
     }
 
     @Override
-    public void onAttachedToActivity(ActivityPluginBinding binding) {
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
         activity = binding.getActivity();
         binding.addActivityResultListener(this);
-//        dropIn.onAttachedToActivity(binding);
     }
 
     @Override
     public void onDetachedFromActivityForConfigChanges() {
         activity = null;
-//        dropIn.onDetachedFromActivity();
     }
 
     @Override
-    public void onReattachedToActivityForConfigChanges(ActivityPluginBinding binding) {
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
         activity = binding.getActivity();
         binding.addActivityResultListener(this);
-//        dropIn.onReattachedToActivityForConfigChanges(binding);
     }
 
     @Override
     public void onDetachedFromActivity() {
         activity = null;
-//        dropIn.onDetachedFromActivity();
     }
 
     @Override
@@ -84,72 +70,77 @@ public class FlutterBraintreePlugin implements FlutterPlugin, ActivityAware, Met
         }
         activeResult = result;
 
-        if (call.method.equals("tokenizeCreditCard")) {
-            String authorization = call.argument("authorization");
-            Intent intent = new Intent(activity, FlutterBraintreeCustom.class);
-            intent.putExtra("type", "tokenizeCreditCard");
-            intent.putExtra("authorization", (String) call.argument("authorization"));
-            assert (call.argument("request") instanceof Map);
-            Map request = (Map) call.argument("request");
-            intent.putExtra("cardNumber", (String) request.get("cardNumber"));
-            intent.putExtra("expirationMonth", (String) request.get("expirationMonth"));
-            intent.putExtra("expirationYear", (String) request.get("expirationYear"));
-            intent.putExtra("cvv", (String) request.get("cvv"));
-            activity.startActivityForResult(intent, CUSTOM_ACTIVITY_REQUEST_CODE);
-        } else if (call.method.equals("requestPaypalNonce")) {
-            String authorization = call.argument("authorization");
-            Intent intent = new Intent(activity, FlutterBraintreeCustom.class);
-            intent.putExtra("type", "requestPaypalNonce");
-            intent.putExtra("authorization", (String) call.argument("authorization"));
-            assert (call.argument("request") instanceof Map);
-            Map request = (Map) call.argument("request");
-            intent.putExtra("amount", (String) request.get("amount"));
-            intent.putExtra("currencyCode", (String) request.get("currencyCode"));
-            intent.putExtra("displayName", (String) request.get("displayName"));
-            intent.putExtra("billingAgreementDescription", (String) request.get("billingAgreementDescription"));
-            activity.startActivityForResult(intent, CUSTOM_ACTIVITY_REQUEST_CODE);
-//            String amount = (String) request.get("amount");
-//            if(amount ==null){
-//                paypalVaultFlow(call);
-//            }else{
-//                paypalCheckOutFlow(call);
-//            }
-
-        } else if (call.method.equals("isApplePayAvailable")) {
-            result.success(false);
-        } else if (call.method.equals("isGooglePayAvailable")) {
-
-            Intent intent = new Intent(activity, FlutterBraintreeCustom.class);
-            intent.putExtra("type", "isGooglePayAvailable");
-            intent.putExtra("authorization", (String) call.argument("authorization"));
-            activity.startActivityForResult(intent, CUSTOM_ACTIVITY_REQUEST_CODE);
-        }  else if (call.method.equals("collectDeviceData")) {
-
-            Intent intent = new Intent(activity, FlutterBraintreeCustom.class);
-            intent.putExtra("type", "collectDeviceData");
-            intent.putExtra("authorization", (String) call.argument("authorization"));
-            activity.startActivityForResult(intent, CUSTOM_ACTIVITY_REQUEST_CODE);
-        } else if (call.method.equals("payWithGooglePay")) {
-
-            Intent intent = new Intent(activity, FlutterBraintreeCustom.class);
-            intent.putExtra("type", "payWithGooglePay");
-            intent.putExtra("authorization", (String) call.argument("authorization"));
-            intent.putExtra("testing", (boolean) call.argument("testing"));
-            intent.putExtra("label", (String) call.argument("label"));
-            intent.putExtra("currencyCode", (String) call.argument("currencyCode"));
-            intent.putExtra("total", (String) call.argument("total"));
-            activity.startActivityForResult(intent, CUSTOM_ACTIVITY_REQUEST_CODE);
-        } else {
-            result.notImplemented();
+        if (activity == null) {
+            result.error("no_activity", "Plugin not attached to an activity.", null);
             activeResult = null;
+            return;
+        }
+
+        switch (call.method) {
+            case "tokenizeCreditCard": {
+                Intent intent = new Intent(activity, FlutterBraintreeCustom.class);
+                intent.putExtra("type", "tokenizeCreditCard");
+                intent.putExtra("authorization", (String) call.argument("authorization"));
+                Map request = (Map) call.argument("request");
+                intent.putExtra("cardNumber", (String) request.get("cardNumber"));
+                intent.putExtra("expirationMonth", (String) request.get("expirationMonth"));
+                intent.putExtra("expirationYear", (String) request.get("expirationYear"));
+                intent.putExtra("cvv", (String) request.get("cvv"));
+                activity.startActivityForResult(intent, CUSTOM_ACTIVITY_REQUEST_CODE);
+                break;
+            }
+            case "requestPaypalNonce": {
+                Intent intent = new Intent(activity, FlutterBraintreeCustom.class);
+                intent.putExtra("type", "requestPaypalNonce");
+                intent.putExtra("authorization", (String) call.argument("authorization"));
+                Map request = (Map) call.argument("request");
+                intent.putExtra("amount", (String) request.get("amount"));
+                intent.putExtra("currencyCode", (String) request.get("currencyCode"));
+                intent.putExtra("displayName", (String) request.get("displayName"));
+                intent.putExtra("billingAgreementDescription", (String) request.get("billingAgreementDescription"));
+                activity.startActivityForResult(intent, CUSTOM_ACTIVITY_REQUEST_CODE);
+                break;
+            }
+            case "isApplePayAvailable":
+                result.success(false); // iOS only
+                activeResult = null;
+                break;
+
+            case "isGooglePayAvailable": {
+                Intent intent = new Intent(activity, FlutterBraintreeCustom.class);
+                intent.putExtra("type", "isGooglePayAvailable");
+                intent.putExtra("authorization", (String) call.argument("authorization"));
+                activity.startActivityForResult(intent, CUSTOM_ACTIVITY_REQUEST_CODE);
+                break;
+            }
+            case "collectDeviceData": {
+                Intent intent = new Intent(activity, FlutterBraintreeCustom.class);
+                intent.putExtra("type", "collectDeviceData");
+                intent.putExtra("authorization", (String) call.argument("authorization"));
+                activity.startActivityForResult(intent, CUSTOM_ACTIVITY_REQUEST_CODE);
+                break;
+            }
+            case "payWithGooglePay": {
+                Intent intent = new Intent(activity, FlutterBraintreeCustom.class);
+                intent.putExtra("type", "payWithGooglePay");
+                intent.putExtra("authorization", (String) call.argument("authorization"));
+                intent.putExtra("testing", (boolean) call.argument("testing"));
+                intent.putExtra("label", (String) call.argument("label"));
+                intent.putExtra("currencyCode", (String) call.argument("currencyCode"));
+                intent.putExtra("total", (String) call.argument("total"));
+                activity.startActivityForResult(intent, CUSTOM_ACTIVITY_REQUEST_CODE);
+                break;
+            }
+            default:
+                result.notImplemented();
+                activeResult = null;
+                break;
         }
     }
 
     @Override
     public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        if (activeResult == null)
-            return false;
+        if (activeResult == null) return false;
 
         if (requestCode == CUSTOM_ACTIVITY_REQUEST_CODE) {
             if (resultCode == Activity.RESULT_OK) {
@@ -165,8 +156,7 @@ public class FlutterBraintreePlugin implements FlutterPlugin, ActivityAware, Met
                         activeResult.success(data.getStringExtra("result"));
                         break;
                     default:
-                        Exception error = new Exception("Invalid activity result type.");
-                        activeResult.error("error", error.getMessage(), null);
+                        activeResult.error("error", "Invalid activity result type.", null);
                         break;
                 }
             } else if (resultCode == Activity.RESULT_CANCELED) {
@@ -178,6 +168,7 @@ public class FlutterBraintreePlugin implements FlutterPlugin, ActivityAware, Met
             activeResult = null;
             return true;
         }
+
         return false;
     }
 }
